0e8d541b6d32c868d89a44adb911b65d15542d41
josue <jcalderonv05@gmail.com>
2018-10-30 03:23:01 -0600

    Add translation POJO

---
+++ ExamenPoliBot/Entities_POJO/Translation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Entities_POJO
+{
+    public class Translation : BaseEntity
+    {
+        public Translation()
+        {
+        }
+
+        public Translation(string[] info)
+        {
+            if (info != null && info.Length >= 6)
+            {
+                User = info[0];
+                if (DateTime.TryParse(info[1], out var date))
+                    Date = date;
+                else
+                    throw new Exception("Date must be a date");
+                Language = info[2];
+                SpanishSentence = info[3];
+                TranslatedSentence = info[4];
+                Popularity = 0;
+            }
+            else
+            {
+                throw new Exception("All values are required");
+            }
+
+        }
+
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public string Language { get; set; }
+        public string SpanishSentence { get; set; }
+        public string TranslatedSentence { get; set; }
+        public int Popularity { get; set; }
+
+    }
+}

36dc03995d0b5fd2dd1202f6691247a7c3f50f4b
josue <jcalderonv05@gmail.com>
2018-10-30 02:40:27 -0600

    WordManager

--- ExamenPoliBot/CoreApi/WordManager.cs
+++ ExamenPoliBot/CoreApi/WordManager.cs
@@ -1,12 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DataAccess.Crud;
+using Entities_POJO;
+using Exceptions;
 
 namespace CoreApi
 {
-    class WordManager
+    class WordManager : BaseManager
     {
+
+        private WordCrudFactory crudWord;
+
+        public WordManager()
+        {
+            crudWord = new WordCrudFactory();
+        }
+
+        public void Create(Word word)
+        {
+            try
+            {
+                var u = crudWord.Retrieve<Word>(word);
+
+                if (u != null)
+                {
+                    //Word already exist
+                    throw new BusinessException(4);
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.GetInstance().Process(ex);
+            }
+        }
+
+        public List<Word> RetrieveAll()
+        {
+            return crudWord.RetrieveAll<Word>();
+        }
+
+        public Word RetrieveById(Word word)
+        {
+            Word w = null;
+            try
+            {
+                w = crudWord.Retrieve<Word>(word);
+                if (w == null)
+                {
+                    throw new BusinessException(5);
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.GetInstance().Process(ex);
+            }
+
+            return w;
+        }
+
+        public void Update(Language language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Language language)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
